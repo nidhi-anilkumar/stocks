6cca2a6f7a5bd5ad97f19f46acdb69aa806ea852
rahul <rahuljaiswal19890711@gmail.com>
2020-02-23 10:12:02 -0800

    Stocks data download trial#1
    Signed-off-by: rahul <rahuljaiswal19890711@gmail.com>

---
+++ download_data.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('wget https://query1.finance.yahoo.com/v7/finance/download/CGC?period1=1550917039&period2=1582453039&interval=1d&events=history&crumb=NcjfvLaKudv --user=remote_user --ask-password')
